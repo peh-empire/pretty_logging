fd09b0ef275e80ee8b88ad7441007b407e2b6264
peh-empire <57708857+peh-empire@users.noreply.github.com>
2020-07-30 08:22:44 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
 from setuptools import setup
 
 setup(
-    name='pretty',
+    name='pretty_logging',
     version='0.1',
     description='Package for debug prints',
     url='git@github.com:peh-empire/pretty-debug.git',
     author='Tobias Locker',
     author_email='tobias@tobiaslocker.de',
     license='unlicensed',
-    packages=['pretty'],
+    packages=['pretty_logging'],
     scripts=[],
     install_requires=['pandas'],
     zip_safe=False,
