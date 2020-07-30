24c4542314dc2abf5df3ddc9d113198d9d5b270b
Tobias Locker <tobias@tobiaslocker.de>
2020-07-30 07:17:09 +0100

    add set level

--- pretty/__init__.py
+++ pretty/__init__.py
@@ -14,6 +14,9 @@ logging.basicConfig(
         datefmt='%y-%m-%d %H:%M:%S')
 
 
+def set_level(level):
+    logger.setLevel(level)
+
 def eprint(*args, **kwargs):
     print(*args, file=sys.stderr, **kwargs)
 
@@ -26,17 +29,9 @@ def info(msg):
 def debug(msg):
     logger.debug(msg)
 
-def pretty_debug(func):
+
+def timeit(func):
     def wrap(*args):
-        sig = inspect.signature(func)
-        function_file = os.path.basename(inspect.getfile(func))
-        logger.debug(f'{function_file} {func.__name__}{sig}')
-        for name, value in dict(zip(sig.parameters.items(), args)).items():
-            if isinstance(value, pd.DataFrame) or isinstance(value, pd.Series):
-                eprint(f'  ---> {name[0]}:')
-                eprint(f'{value}')
-            else:
-                eprint(f'  ---> {name[0]} = {value}')
         start = datetime.datetime.now()
         f = func(*args)
         end = datetime.datetime.now()
@@ -44,4 +39,27 @@ def pretty_debug(func):
         total = total.microseconds
         eprint(f'  {func.__name__} took: {total} microseconds')
         return f
+    return wrap
+
+
+def pretty_debug(func):
+    def wrap(*args):
+        sig = inspect.signature(func)
+        function_file = os.path.basename(inspect.getfile(func))
+        logger.debug(f'{function_file} {func.__name__}{sig}')
+        if logger.getEffectiveLevel() == logging.DEBUG:
+            for name, value in dict(zip(sig.parameters.items(), args)).items():
+                if isinstance(value, pd.DataFrame) or isinstance(value, pd.Series):
+                    eprint(f'  ---> {name[0]}:')
+                    eprint(f'{value}')
+                else:
+                    eprint(f'  ---> {name[0]} = {value}')
+            start = datetime.datetime.now()
+            f = func(*args)
+            end = datetime.datetime.now()
+            total = end - start
+            total = total.microseconds
+            eprint(f'  {func.__name__} took: {total} microseconds')
+            return f
+        return func(*args)
     return wrap
